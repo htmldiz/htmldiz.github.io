dc24a0e41865d3617047de9fe800bc2f2c4d5f4d
htmldiz <mark.tanasevich@gmal.com>
2018-07-05 15:29:16 +0300

    update back

--- c1.js
+++ c1.js
@@ -145,6 +145,7 @@ $(function() {
         var us3     = localStorage.getItem('usc4');
         visitThisPage = $.fn.visitThisPage();
         if (v3 > window.mvc4 && us3 == 'false' && tabs == 1 && window.candetect == true) {
+        	alert('detect');
           showPopup();
           window.mmd = 0;
         }
