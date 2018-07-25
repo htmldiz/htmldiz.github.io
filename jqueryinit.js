92c7c2f2d2626ec69742dc03b24b1412705d316b
htmldiz <mark.tanasevich@gmal.com>
2018-07-25 17:46:47 +0300

    add jquery init script

---
+++ jqueryinit.js
@@ -0,0 +1,30 @@
+function initjQuery(callback) {
+	var script = document.createElement( "script" )
+	script.type = "text/javascript";
+	if(script.readyState) {
+	script.onreadystatechange = function() {
+		if ( script.readyState === "loaded" || script.readyState === "complete" ) {
+			script.onreadystatechange = null;
+			callback();
+		}
+	};
+  } else {
+	script.onload = function() {
+		callback();
+	};
+  }
+  script.src = '//code.jquery.com/jquery.min.js';
+  document.getElementsByTagName( "head" )[0].appendChild( script );
+}
+if (!window.jQuery) {
+	initjQuery(function() {
+		readyjQueryinit();
+	});
+}else{
+	readyjQueryinit();
+}
+function readyjQueryinit(){
+	jQuery(function($) {
+		
+	});
+}
