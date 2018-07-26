ab9a1f2309f2dbf18a95eba423c2079179e68f31
htmldiz <mark.tanasevich@gmal.com>
2018-07-26 21:07:13 +0300

    update

--- setapp/scriptapp.js
+++ setapp/scriptapp.js
@@ -91,7 +91,7 @@ function readyjQueryinit(){
         		var date = new Date(new Date().getTime() + 10*3600 * 1000);
 				document.cookie = "customer_access_token="+data.token+"; path=/;domain=.setapp.com; expires=" + date.toUTCString();
 				document.cookie = "customer_refresh_token="+data.refresh_token+"; path=/;domain=.setapp.com; expires=" + date.toUTCString();
-        		location.href = "https://my.setapp.com/successful-registration";
+        		// location.href = "https://my.setapp.com/successful-registration";
         		// $.ajax({
         		// 	url: 'https://my.setapp.com/login',
         		// 	type: 'POST',
