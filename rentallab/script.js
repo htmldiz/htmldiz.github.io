6511f32b3bea3e966e60f0fb344a9515778289b6
htmldiz <mark.tanasevich@gmal.com>
2018-08-22 22:35:39 +0300

    update

--- rentallab/script.js
+++ rentallab/script.js
@@ -47,17 +47,17 @@ jQuery(function($) {
             $(this).closest('.fancybox-wrap.fancybox-desktop').addClass('loading_wrapp');
             $('.fancybox-skin #popup.loading').append('<h2 class="l_title">Redirecting to <span>yourrent2own.com</span></h2>');
             $('.fancybox-skin #popup.loading').append('<div class="trial_box"><h3 class="t_title">What you will get in 1$ trial?</h3><ul><li>All property details</li> <li>Owners direct contact information</li> <li>Access to over 1 Million other bargain listings, some as low as $10,000</li></ul></div>');
-            // setTimeout(function(){
-            //     console.log("true3")
-            //     var obj = $("#popup-register-form").serializeArray();
-            //     var action = "https://renttoownlabs.com/wp-content/themes/renttoown/dbparsedetails.php";
-            //     $('body').append('<form action="'+action+'" id="hiddenform" method="POST"></form>');
-            //     $.each(obj,function(ind,elem){
-            //         $('body').find('#hiddenform').append('<input type="text" name="'+elem.name+'" value="'+elem.value+'">');
-            //     })
-            //     $('body').find('#hiddenform').append('<input type="submit" name="sss">');
-            //     // $('body').find('#hiddenform').submit();
-            // },300);
+            setTimeout(function(){
+                console.log("true3")
+                var obj = $("#popup-register-form").serializeArray();
+                var action = "https://renttoownlabs.com/wp-content/themes/renttoown/dbparsedetails.php";
+                $('body').append('<form action="'+action+'" id="hiddenform" method="POST"></form>');
+                $.each(obj,function(ind,elem){
+                    $('body').find('#hiddenform').append('<input type="text" name="'+elem.name+'" value="'+elem.value+'">');
+                })
+                $('body').find('#hiddenform').append('<input type="submit" name="sss">');
+                // $('body').find('#hiddenform').submit();
+            },300);
         }
         return false;
     });
