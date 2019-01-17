8bc7c3bc11a30686aee1834ba6aa424c0bf54d05
htmldiz <mark.tanasevich@gmail.com>
2019-01-17 22:24:01 +0300

    update

---
+++ prom_tests/prom.js
@@ -0,0 +1,31 @@
+function initjQuery(callback) {
+	var script = document.createElement( "script" )
+	script.type = "text/javascript";
+	if(script.readyState) {
+	script.onreadystatechange = function() {
+		if ( script.readyState === "loaded" || script.readyState === "complete" ) {
+			script.onreadystatechange = null;
+			callback();
+		}
+	};
+  } else {
+	script.onload = function() {
+		callback();
+	};
+  }
+  script.src = '//code.jquery.com/jquery-3.3.1.min.js';
+  document.getElementsByTagName( "head" )[0].appendChild( script );
+}
+if (!window.jQuery) {
+	initjQuery(function() {
+		readyjQueryinit();
+	});
+}else{
+	readyjQueryinit();
+}
+var hrf = location.protocol+'//'+location.host+location.pathname;
+function readyjQueryinit(){
+	jQuery(document).ready(function($) {
+		$('.x-region-selector__delivery-holder').find('input[type="checkbox"]').change();
+	});
+}
