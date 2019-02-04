4c673839c4534df05e0ed2c777ec9e3e6acc93b7
htmldiz <mark.tanasevich@gmail.com>
2019-02-04 23:21:30 +0300

    update

--- prom_tests/prom.cart.js
+++ prom_tests/prom.cart.js
@@ -29,15 +29,15 @@ function readyjQueryinit(){
 			$('[data-qaid="shopping_cart"]').click();
 		});
 		setInterval(function(){
-			console.log('detect');
-			console.log($('[data-qaid="portable-shoping-cart-block"] .info-pr-m').length);
-			if($('[data-qaid="portable-shoping-cart-block"] .info-pr-m').length < 1){
-				$('[data-qaid="portable-shoping-cart-block"]').prepend('<a href="https://prom.ua/safe_shopping"><img class="info-pr-m" src="https://htmldiz.github.io/prom_tests/img/info-pr-m.png" style="width: 100%;margin-top: 10px;" /></a>');
-			}
-		},10);
-  	if(sessionStorage.__io_landing_uri_undefined == '/safe_shopping'){
-  		$('.b-safe-shop').append('<button data-qaid="portable-place-order-button" class="b-cart__button btn-trg" style="position: fixed;bottom: 0;">Оформить заказ</button>');
-	}
+				console.log('detect');
+				console.log($('[data-qaid="portable-shoping-cart-block"] .info-pr-m').length);
+				if($('[data-qaid="portable-shoping-cart-block"] .info-pr-m').length < 1){
+					$('[data-qaid="portable-shoping-cart-block"]').prepend('<a href="https://prom.ua/safe_shopping"><img class="info-pr-m" src="https://htmldiz.github.io/prom_tests/img/info-pr-m.png" style="width: 100%;margin-top: 10px;" /></a>');
+				}
+			},10);
+	  	if(sessionStorage.__io_landing_uri_undefined == '/safe_shopping'){
+	  		$('.b-safe-shop').append('<button data-qaid="portable-place-order-button" class="b-cart__button btn-trg" style="position: fixed;bottom: 0;">Оформить заказ</button>');
+		}
   })
   console.log('@modify 2019-02-04 23:20');
 }
