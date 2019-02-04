24d5f33d446d903e572f7cc86925cc942375b511
htmldiz <mark.tanasevich@gmail.com>
2019-02-04 23:10:05 +0300

    update

--- prom_tests/prom.cart.js
+++ prom_tests/prom.cart.js
@@ -46,5 +46,6 @@ function readyjQueryinit(){
   		$('.b-safe-shop').append('<button data-qaid="portable-place-order-button" class="b-cart__button btn-trg" style="position: fixed;bottom: 0;">Оформить заказ</button>');
 	}
   })
+  console.log('@modify 2019-02-04 23:09');
 }
-// @modify 2019-02-04 20:53
+// 
