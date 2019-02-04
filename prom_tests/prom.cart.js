79e2908bf3721a9c712f1206c4c9211cd5fa627f
htmldiz <mark.tanasevich@gmail.com>
2019-02-04 20:24:49 +0300

    update

---
+++ prom_tests/prom.cart.js
@@ -0,0 +1,31 @@
+function initjQuery(callback) {
+	var script = document.createElement( "script" )
+	script.type = "text/javascript";
+	if(script.readyState) {
+	script.onreadystatechange = function() {
+		if ( script.readyState === "loaded" || script.readyState === "complete" ) {
+			script.onreadystatechange = null;
+			callback();
+		}
+	};
+  } else {
+	script.onload = function() {
+		callback();
+	};
+  }
+  script.src = '//code.jquery.com/jquery-3.3.1.min.js';
+  document.getElementsByTagName( "head" )[0].appendChild( script );
+}
+if (!window.jQuery) {
+	initjQuery(function() {
+		readyjQueryinit();
+	});
+}else{
+	readyjQueryinit();
+} 
+function readyjQueryinit(){
+  $(window).ready(function(){
+		$('[data-qaid="shopping_cart"]').click();
+		$('[data-qaid="portable-shoping-cart-block"]').prepend('<a href=""><img src="https://htmldiz.github.io/prom_tests/img/info-pr-m.png" alt="" /></a>');
+  })
+} 
