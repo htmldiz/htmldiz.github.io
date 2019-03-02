6ce0cafb32f6a88a18e7eb284e7b0b5da32325ee
bogdanmir <muronhtz@gmail.com>
2019-03-02 03:00:43 +0200

    Upd

---
+++ prom_tests/prom.checkout.desktop.js
@@ -0,0 +1,34 @@
+try {
+    hj('trigger', 'exp-checkout');
+} 
+catch(e) {} 
+function initjQuery(callback) {
+	var script = document.createElement( "script" )
+	script.type = "text/javascript";
+	if(script.readyState) {
+	script.onreadystatechange = function() {
+		if ( script.readyState === "loaded" || script.readyState === "complete" ) {
+			script.onreadystatechange = null;
+			callback();
+		}
+	};
+  } else {
+	script.onload = function() {
+		callback();
+	};
+  }
+  script.src = '//code.jquery.com/jquery-3.3.1.min.js';
+  document.getElementsByTagName( "head" )[0].appendChild( script );
+}
+if (!window.jQuery) {
+	initjQuery(function() {
+		readyjQueryinit();
+	});
+}else{
+	readyjQueryinit();
+} 
+function readyjQueryinit(){
+  $(window).ready(function(){
+
+  })
+}
