2ec4d423eed886fccce065e693c0916bdbd18a81
htmldiz <mark.tanasevich@gmail.com>
2019-08-23 13:19:46 +0300

    update

--- setapp/newversion/script.js
+++ setapp/newversion/script.js
@@ -1834,16 +1834,6 @@ $('body').on('click', '[data-scrollto-element]', function(event) {
 });
 var tmr = setInterval(function(){
   if($('html').find('.all-apps-item').length > 0){
-  	if($('.successful-registration').length > 0){
-  		var src  = $('.successful-registration .successful-registration-from-app-landing__app-description .successful-registration-from-app-landing__app-icon').attr('src');
-  		var h4   = $('.successful-registration-from-app-landing__app-description .h4').text();
-  		var lead = $('.successful-registration-from-app-landing__app-description .lead').text();
-  		$('.apps-subtitle').find('.application-item img').attr('src',src);
-  		$('.contentin-logo .istat-img img').attr('src',src);
-  		$('.apps-subtitle').find('.all-apps-item__name').html(h4);
-  		$('.apps-subtitle').find('.application-item img').attr('alt',h4);
-  		$('.apps-subtitle').find('.all-apps-item__description').html(lead); 
-  	} 
     clearInterval(tmr);
     $('body').find('.all-apps-item').each(function(index, el) {
       $(el).find('.all-apps-item__description').after('<a href="#" class="download-btn">Free Download</a>');
@@ -1864,6 +1854,16 @@ var tmr = setInterval(function(){
           $('[href="#'+idtohref+'"]').addClass('-active');
         }
       }
+    // if($('.successful-registration').length > 0){
+      var src  = $('.successful-registration .successful-registration-from-app-landing__app-description .successful-registration-from-app-landing__app-icon').attr('src');
+      var h4   = $('.successful-registration-from-app-landing__app-description .h4').text();
+      var lead = $('.successful-registration-from-app-landing__app-description .lead').text();
+      $('.apps-subtitle').find('.application-item img').attr('src',src);
+      $('.contentin-logo .istat-img img').attr('src',src);
+      $('.apps-subtitle').find('.all-apps-item__name').html(h4);
+      $('.apps-subtitle').find('.application-item img').attr('alt',h4);
+      $('.apps-subtitle').find('.all-apps-item__description').html(lead); 
+    // }
     });
   }
 },100)
