6574771b9513a67de485acdd82cdca86c52aa54c
htmldiz <mark.tanasevich@gmail.com>
2019-09-04 17:14:43 +0300

    update

---
+++ cooksmarts/cooksmarts.js
@@ -0,0 +1,28 @@
+function initjQuery(callback) {
+	var script = document.createElement( "script" )
+	script.type = "text/javascript";
+	if(script.readyState) {
+	script.onreadystatechange = function() {
+		if ( script.readyState === "loaded" || script.readyState === "complete" ) {
+			script.onreadystatechange = null;
+			callback();
+		}
+	};
+  } else {
+	script.onload = function() {
+		callback();
+	};
+  }
+  script.src = '//code.jquery.com/jquery-3.3.1.min.js';
+  document.getElementsByTagName( "head" )[0].appendChild( script );
+}
+if (!window.jQuery) {
+	initjQuery(function() {
+		readyjQueryinit();
+	});
+}else{
+	readyjQueryinit();
+} 
+function readyjQueryinit(){
+console.log('%cTest is come!',"color:red;")
+}
