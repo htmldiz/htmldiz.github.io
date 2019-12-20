d0403aaced52d832d296687c60c01e4b9c97d2ab
bogdanmir <muronhtz@gmail.com>
2019-12-20 13:32:46 +0200

    Upd

---
+++ kabanchik/add_top_specialists_block_on_the_order_form.js
@@ -0,0 +1,350 @@
+function initjQuery(callback) {
+	var script = document.createElement( "script" )
+	script.type = "text/javascript";
+	if(script.readyState) {
+	script.onreadystatechange = function() {
+		if ( script.readyState === "loaded" || script.readyState === "complete" ) {
+			script.onreadystatechange = null;
+			callback();
+		}
+	};
+  } else {
+	script.onload = function() {
+		callback();
+	};
+  }
+  script.src = '//code.jquery.com/jquery-3.3.1.min.js';
+  document.getElementsByTagName( "head" )[0].appendChild( script );
+}
+if (!window.jQuery) {
+	initjQuery(function() {
+		readyjQueryinit();
+	});
+}else{
+	readyjQueryinit();
+}
+function readyjQueryinit(){
+  $(window).ready(function(){
+
+
+			$outhtml  = '<div class="wrap_ab_block">';
+			$outhtml += '<h3 class="title_ab_block">Топ-специалисты в этой категории:</h3>';
+
+			$outhtml += '<div class="item_ab_block">';
+$outhtml += '<div class="kb-top-performer__collapsible-holder">'
+$outhtml += '    <div class="kb-top-performer__wrapper">'
+$outhtml += '        <div class="kb-top-performer__content">'
+$outhtml += '            <div class="kb-top-performer__user">'
+$outhtml += '                <div class="kb-top-performer__avatar">'
+$outhtml += '                    <div class="b-rounded-image b-image-holder">'
+$outhtml += '                        <a class="targetNew" href="https://kabanchik.ua/user/1300124">'
+$outhtml += '                            <img class="b-image-holder__img" src="https://images.kabanchik.ua/6c40cd2a-af1f-47fe-b005-39be6e0e6d83.jpg" alt="Павло Б." title="Павло Б.">'
+$outhtml += '                        </a>'
+$outhtml += '                    </div>'
+$outhtml += '                </div>'
+$outhtml += '                <div class="kb-top-performer__user-info">'
+$outhtml += '                    <div class="kb-top-performer__name-holder">'
+$outhtml += '                        <a class="kb-top-performer__name kb-link kb-link_color_content targetNew" href="https://kabanchik.ua/user/1300124">Павло Б.</a>'
+$outhtml += '                    </div>'
+$outhtml += '                </div>'
+$outhtml += '            </div>'
+$outhtml += '        </div>'
+$outhtml += '        <div class="kb-top-performer__sidebar">'
+$outhtml += '            <div class="kb-top-performer__assign-button kb-top-performer__assign-button_type_sidebar">'
+$outhtml += '                <a class="b-button b-button_width_full targetNew" href="https://kabanchik.ua/task/assign/947721/cid/126"><span class="b-button__text">Предложить работу</span></a>'
+$outhtml += '            </div>'
+$outhtml += '            <div class="kb-top-performer__reviews-stats kb-top-performer__reviews-stats_type_sidebar">'
+$outhtml += '                <div class="kb-reviews-stats kb-reviews-stats_size_small kb-reviews-stats_type_listing-performer">'
+$outhtml += '                    <div class="kb-reviews-stats__item" >'
+$outhtml += '                        <div class="kb-reviews-stats__count">19</div>'
+$outhtml += '                        <a class="targetNew" href="https://kabanchik.ua/user/1300124#reviews">отзыва</a>'
+$outhtml += '                    </div>'
+$outhtml += '                    <div class="kb-reviews-stats__item">'
+$outhtml += '                        <div class="kb-reviews-stats__count">92%</div>'
+$outhtml += '                        <span>положительных</span>'
+$outhtml += '                    </div>'
+$outhtml += '                </div>'
+$outhtml += '            </div>'
+$outhtml += '        </div>'
+$outhtml += '    </div>'
+$outhtml += '</div>'
+$outhtml += '</div>'
+
+			$outhtml += '<div class="item_ab_block">';
+$outhtml += '<div class="kb-top-performer__collapsible-holder">'
+$outhtml += '    <div class="kb-top-performer__wrapper">'
+$outhtml += '        <div class="kb-top-performer__content">'
+$outhtml += '            <div class="kb-top-performer__user">'
+$outhtml += '                <div class="kb-top-performer__avatar">'
+$outhtml += '                    <div class="b-rounded-image b-image-holder">'
+$outhtml += '                        <a class="targetNew" href="https://kabanchik.ua/user/50550">'
+$outhtml += '                            <img class="b-image-holder__img" src="https://images.kabanchik.ua/8c5c9317-b286-4565-807d-847df3d65cc2.jpg" alt="Алексей Б." title="Алексей Б.">'
+$outhtml += '                        </a>'
+$outhtml += '                    </div>'
+$outhtml += '                    <span class="kb-top-performer__badge"><span class="kb-top-badge"><img class="kb-top-badge__medal" src="https://hoggy.c.prom.st/static/images/medal.image-hc06df3454d3eb2160cbda148ede0ba5b4.svg" width="30" height="38"><span class="kb-top-badge__score">47</span></span></span>'
+$outhtml += '                </div>'
+$outhtml += '                <div class="kb-top-performer__user-info">'
+$outhtml += '                    <div class="kb-top-performer__name-holder">'
+$outhtml += '                        <a class="kb-top-performer__name kb-link kb-link_color_content targetNew" href="https://kabanchik.ua/user/50550">Алексей Б.</a>'
+$outhtml += '                    </div>'
+$outhtml += '                </div>'
+$outhtml += '            </div>'
+$outhtml += '        </div>'
+$outhtml += '        <div class="kb-top-performer__sidebar">'
+$outhtml += '            <div class="kb-top-performer__assign-button kb-top-performer__assign-button_type_sidebar">'
+$outhtml += '                <a class="b-button b-button_width_full targetNew" href="https://kabanchik.ua/task/assign/35065/cid/126"><span class="b-button__text">Предложить работу</span></a>'
+$outhtml += '            </div>'
+$outhtml += '            <div class="kb-top-performer__reviews-stats kb-top-performer__reviews-stats_type_sidebar">'
+$outhtml += '                <div class="kb-reviews-stats kb-reviews-stats_size_small kb-reviews-stats_type_listing-performer">'
+$outhtml += '                    <div class="kb-reviews-stats__item" >'
+$outhtml += '                        <div class="kb-reviews-stats__count">452</div>'
+$outhtml += '                        <a class="targetNew" href="https://kabanchik.ua/user/50550#reviews">отзыва</a>'
+$outhtml += '                    </div>'
+$outhtml += '                    <div class="kb-reviews-stats__item">'
+$outhtml += '                        <div class="kb-reviews-stats__count">98%</div>'
+$outhtml += '                        <span>положительных</span>'
+$outhtml += '                    </div>'
+$outhtml += '                </div>'
+$outhtml += '            </div>'
+$outhtml += '        </div>'
+$outhtml += '    </div>'
+$outhtml += '</div>'
+$outhtml += '</div>'
+
+			$outhtml += '<div class="item_ab_block">';
+$outhtml += '<div class="kb-top-performer__collapsible-holder">'
+$outhtml += '    <div class="kb-top-performer__wrapper">'
+$outhtml += '        <div class="kb-top-performer__content">'
+$outhtml += '            <div class="kb-top-performer__user">'
+$outhtml += '                <div class="kb-top-performer__avatar">'
+$outhtml += '                    <div class="b-rounded-image b-image-holder">'
+$outhtml += '                        <a class="targetNew" href="https://kabanchik.ua/user/201738">'
+$outhtml += '                            <img class="b-image-holder__img" src="https://images.kabanchik.ua/de0b07bf-7d6c-4572-9d1e-7b62bcf01187.jpg" alt="Александр К." title="Александр К.">'
+$outhtml += '                        </a>'
+$outhtml += '                    </div>'
+$outhtml += '                    <span class="kb-top-performer__badge"><span class="kb-top-badge"><img class="kb-top-badge__medal" src="https://hoggy.c.prom.st/static/images/medal.image-hc06df3454d3eb2160cbda148ede0ba5b4.svg" width="30" height="38"><span class="kb-top-badge__score">24</span></span></span>'
+$outhtml += '                </div>'
+$outhtml += '                <div class="kb-top-performer__user-info">'
+$outhtml += '                    <div class="kb-top-performer__name-holder">'
+$outhtml += '                        <a class="kb-top-performer__name kb-link kb-link_color_content targetNew" href="https://kabanchik.ua/user/201738">Александр К.</a>'
+$outhtml += '                    </div>'
+$outhtml += '                </div>'
+$outhtml += '            </div>'
+$outhtml += '        </div>'
+$outhtml += '        <div class="kb-top-performer__sidebar">'
+$outhtml += '            <div class="kb-top-performer__assign-button kb-top-performer__assign-button_type_sidebar">'
+$outhtml += '                <a class="b-button b-button_width_full targetNew" href="https://kabanchik.ua/task/assign/150038/cid/126"><span class="b-button__text">Предложить работу</span></a>'
+$outhtml += '            </div>'
+$outhtml += '            <div class="kb-top-performer__reviews-stats kb-top-performer__reviews-stats_type_sidebar">'
+$outhtml += '                <div class="kb-reviews-stats kb-reviews-stats_size_small kb-reviews-stats_type_listing-performer">'
+$outhtml += '                    <div class="kb-reviews-stats__item" >'
+$outhtml += '                        <div class="kb-reviews-stats__count">378</div>'
+$outhtml += '                        <a class="targetNew" href="https://kabanchik.ua/user/201738#reviews">отзыва</a>'
+$outhtml += '                    </div>'
+$outhtml += '                    <div class="kb-reviews-stats__item">'
+$outhtml += '                        <div class="kb-reviews-stats__count">93%</div>'
+$outhtml += '                        <span>положительных</span>'
+$outhtml += '                    </div>'
+$outhtml += '                </div>'
+$outhtml += '            </div>'
+$outhtml += '        </div>'
+$outhtml += '    </div>'
+$outhtml += '</div>'
+$outhtml += '</div>'
+
+			$outhtml += '<div class="item_ab_block">';
+$outhtml += '<div class="kb-top-performer__collapsible-holder">'
+$outhtml += '    <div class="kb-top-performer__wrapper">'
+$outhtml += '        <div class="kb-top-performer__content">'
+$outhtml += '            <div class="kb-top-performer__user">'
+$outhtml += '                <div class="kb-top-performer__avatar">'
+$outhtml += '                    <div class="b-rounded-image b-image-holder">'
+$outhtml += '                        <a class="targetNew" href="https://kabanchik.ua/user/866096">'
+$outhtml += '                            <img class="b-image-holder__img" src="https://images.kabanchik.ua/fee0a401-93bd-439d-8eba-5f1963f95dd5.jpeg" alt="Денис В." title="Денис В.">'
+$outhtml += '                        </a>'
+$outhtml += '                    </div>'
+$outhtml += '                </div>'
+$outhtml += '                <div class="kb-top-performer__user-info">'
+$outhtml += '                    <div class="kb-top-performer__name-holder">'
+$outhtml += '                        <a class="kb-top-performer__name kb-link kb-link_color_content targetNew" href="https://kabanchik.ua/user/866096">Денис В.</a>'
+$outhtml += '                    </div>'
+$outhtml += '                </div>'
+$outhtml += '            </div>'
+$outhtml += '        </div>'
+$outhtml += '        <div class="kb-top-performer__sidebar">'
+$outhtml += '            <div class="kb-top-performer__assign-button kb-top-performer__assign-button_type_sidebar">'
+$outhtml += '                <a class="b-button b-button_width_full targetNew" href="https://kabanchik.ua/task/assign/649530/cid/126"><span class="b-button__text">Предложить работу</span></a>'
+$outhtml += '            </div>'
+$outhtml += '            <div class="kb-top-performer__reviews-stats kb-top-performer__reviews-stats_type_sidebar">'
+$outhtml += '                <div class="kb-reviews-stats kb-reviews-stats_size_small kb-reviews-stats_type_listing-performer">'
+$outhtml += '                    <div class="kb-reviews-stats__item" >'
+$outhtml += '                        <div class="kb-reviews-stats__count">143</div>'
+$outhtml += '                        <a class="targetNew" href="https://kabanchik.ua/user/866096#reviews">отзыва</a>'
+$outhtml += '                    </div>'
+$outhtml += '                    <div class="kb-reviews-stats__item">'
+$outhtml += '                        <div class="kb-reviews-stats__count">96%</div>'
+$outhtml += '                        <span>положительных</span>'
+$outhtml += '                    </div>'
+$outhtml += '                </div>'
+$outhtml += '            </div>'
+$outhtml += '        </div>'
+$outhtml += '    </div>'
+$outhtml += '</div>'
+$outhtml += '</div>'
+$outhtml += '<a class="ab_show_all targetNew" href="https://kiev.kabanchik.ua/category/remont-bitovoy-tehniki">Показать еще</a>'
+
+
+			$outhtml  += '</div>';
+			$('.kb-task-create__item.kb-task-create__item_state_active.qa-task-details').append($outhtml);
+			setTimeout(function(){ 
+				var spaceTop = $(".kb-info-panel.kb-info-panel_pb_0").outerHeight(true) +  $(".kb-task-create__item.kb-task-create__item_state_passed-step").outerHeight(true)
+				console.log(spaceTop)
+				var	heightAbBlock = $(".kb-task-create").height() - spaceTop - 25
+				console.log(heightAbBlock)
+				$(".wrap_ab_block").css("height",heightAbBlock)
+			}, 30);
+
+
+			$('a.targetNew').click(function(event) {
+				event.preventDefault();
+				var width = window.innerWidth * 0.66 ;
+				var height = width * window.innerHeight / window.innerWidth ;
+				window.open(this.href , 'newwindow', 'width=' + width + ', height=' + height + ', top=' + ((window.innerHeight - height) / 2) + ', left=' + ((window.innerWidth - width) / 2));
+			});
+			// if ($(".kb-info-panel.kb-info-panel_pb_0").length>0) {
+			// 	$(".wrap_ab_block").css("top",spaceTop)
+
+			// }
+			
+			// $('body').on("click", ".topbar_cta", function(event) {
+			// 	event.preventDefault()
+			// 	$("body").addClass("modal_open")
+			// })
+
+			var styles = "<style>";
+			styles += `
+			.kb-task-create__item.kb-task-create__item_state_active.qa-task-details {
+				position: relative;
+			}
+			.kb-task-create>* {
+			    width: calc(100% - 360px);
+			}
+			.kb-task-create__item_state_passed-step {
+				width: 100%;
+			}
+			.title_ab_block {
+			    font-weight: bold;
+			    font-size: 16px;
+			    padding-bottom: 25px;
+			}
+			.kb-info-panel.kb-info-panel_pb_0 {
+				width: 100%;
+			}
+			.wrap_ab_block {
+				width: 320px;
+				position: absolute;
+				height: 100%;
+				left: calc(100% - 10px);
+				top: 0;
+				padding-left: 20px;
+				border-left: 1px solid #E8E8E8;
+				margin-top: 25px;
+			}
+			.kb-estimated-cost {
+    			width: 360px!important;
+			}
+			#price-checkbox-wrapper {
+				flex-wrap: wrap;
+			}
+			#price-checkbox-wrapper .kb-text.kb-text_mt_10 {
+				margin-top: 10px!important;
+			}
+			.kb-task-create-payment__estimated-cost .kb-task-create-payment__attributes {
+				width: 360px;
+				flex-wrap: wrap;
+			}
+			.kb-task-create-payment__attributes .kb-task-create-payment__radio-holder {
+			    margin-bottom: 10px;
+			}
+			.ab_show_all {
+				line-height: 1.8;
+				color: #959595;
+				padding: 7px 45px;
+				border: 1px solid #D3D3D3;
+				border-radius: 6px;
+				display: block;
+				margin: auto;
+				width: 120px;
+				text-align: center;
+			}
+.item_ab_block {
+	padding: 15px 10px;
+	border: 1px solid #D3D3D3;
+	border-radius: 6px;
+	margin-bottom: 20px;
+}
+
+.kb-top-performer__sidebar {
+    width: 125px;
+    padding-left: 10px;
+}
+.kb-top-performer__avatar {
+    width: 60px;
+    padding-right: 15px;
+}
+.kb-top-performer__badge {
+    right: 15px;
+    bottom: auto;
+    width: 18px;
+    top: 35px;
+}
+.kb-top-performer__name {
+	margin-right: 0;
+}
+.kb-top-performer__name-holder {
+	font-size: 12px;
+}
+.kb-top-badge__score {
+    top: 12px;
+    font-size: 8px;
+}
+.b-button_width_full .b-button__text {
+	font-size: 10px;
+}
+.b-button_width_full {
+	height: 30px;
+}
+.kb-reviews-stats_size_small .kb-reviews-stats__count {
+	font-size: 12px;
+}
+.kb-top-performer__reviews-stats_type_sidebar a {
+	font-size: 10px;
+}
+.kb-reviews-stats_size_small .kb-reviews-stats__item {
+	font-size: 10px;
+}
+.kb-reviews-stats_type_listing-performer .kb-reviews-stats__item {
+	 padding: 0 6px;
+}
+.kb-top-performer__assign-button_type_sidebar {
+    margin: 0 0 7px 0;
+}
+.kb-top-performer__reviews-stats {
+	margin-top: 7px;
+}
+.kb-reviews-stats_type_listing-performer .kb-reviews-stats__item:first-child {
+	padding-left: 5px;
+}
+@media(max-width: 1199px) {
+	textarea.b-input {
+		width: auto;
+	}
+}
+			`;
+			styles    += "</style>";
+			jQuery('body').append(styles);
+
+
+
+	});
+}
---
+++ prom_tests/porm-sticky_tags_on_product_listingNew.js
@@ -0,0 +1,243 @@
+try {
+    hj('trigger', 'prom_sticky_tags');
+}
+catch(e) {}
+function initjQuery(callback) {
+	var script = document.createElement( "script" )
+	script.type = "text/javascript";
+	if(script.readyState) {
+	script.onreadystatechange = function() {
+		if ( script.readyState === "loaded" || script.readyState === "complete" ) {
+			script.onreadystatechange = null;
+			callback();
+		}
+	};
+  } else {
+	script.onload = function() {
+		callback();
+	};
+  }
+  script.src = '//code.jquery.com/jquery-3.3.1.min.js';
+  document.getElementsByTagName( "head" )[0].appendChild( script );
+}
+if (!window.jQuery) {
+	initjQuery(function() {
+		readyjQueryinit();
+	});
+}else{
+	readyjQueryinit();
+}
+function readyjQueryinit(){
+	jQuery(window).ready(function($){
+		var interval = setInterval(function(){
+		if ($(".promoFilters__root--1Vc6E li").length>0) {
+			$(".promoFilters__root--1Vc6E").appendTo(".x-header__search-row.js-header")
+
+			setInterval(function(){
+				var all_width = 0;
+				
+				var html = $('.x-header__search-row.js-header .promoFilters__root--1Vc6E ul').html();
+				$('.x-header__search-row.js-header .promoFilters__root--1Vc6E').find('li').length
+				$('.x-header__search-row.js-header .promoFilters__root--1Vc6E').find('li').each(function(index, el) {
+					all_width = all_width + $(el).outerWidth()+13;
+				});
+				var realAll_width = all_width
+				$('.x-header__search-row.js-header .promoFilters__root--1Vc6E ul').css('width',realAll_width+'px');
+			}, 50)
+			var trPos = 0;
+			$('body').on('click', '.x-header__search-row.js-header .ab-slider-arrow.left-arrow', function(event) {
+				var space = $('.x-header__search-row.js-header .promoFilters__root--1Vc6E ul').offset().left;
+				if(space < 0 && space < -200){
+					trPos = trPos + 200;
+				}else{
+					if(space < 0){
+						trPos = trPos - space;
+					}
+					$(this).css('display','none');
+				}
+				$('.x-header__search-row.js-header .promoFilters__root--1Vc6E ul').css('transform','translateX('+trPos+'px)');
+				$('body').find('.x-header__search-row.js-header .ab-slider-arrow.right-arrow').attr('style','');
+				return false;
+			});
+			$('body').on('click', '.x-header__search-row.js-header .ab-slider-arrow.right-arrow', function(event) {
+				var space = $('.x-header__search-row.js-header .promoFilters__root--1Vc6E ul').outerWidth()  + $('.x-header__search-row.js-header .promoFilters__root--1Vc6E ul').offset().left - $('.x-header__search-row.js-header .ab-test-slider').outerWidth() + 20;
+				if(space >= 200){
+					trPos = trPos - 200;
+				}else{
+					if(space > 0){
+						trPos = trPos - space;
+					}
+					$(this).css('display','none');
+				}
+					$('.x-header__search-row.js-header .promoFilters__root--1Vc6E ul').css('transform','translateX('+trPos+'px)');
+					$('body').find('.x-header__search-row.js-header .ab-slider-arrow.left-arrow').attr('style','');
+				return false;
+			});
+			$(".promoFilters__root--1Vc6E").wrap("<div class='wrap-ab-test-slider'><div class='ab-test-slider'></div></div>")
+			$(".promoFilters__root--1Vc6E").before('<span class="ab-slider-arrow left-arrow"><svg class="horizontalSlideContent__controlIcon--5mfgs" viewBox="0 0 64 64"><use xlink:href="#right2"></use></svg></span>')
+			$(".promoFilters__root--1Vc6E").after('<span class="ab-slider-arrow right-arrow"><svg class="horizontalSlideContent__controlIcon--5mfgs" viewBox="0 0 64 64"><use xlink:href="#right2"></use></svg></span>')
+			$('.x-header__search-row.js-header').find('.ab-slider-arrow.left-arrow').css('display','none');
+			var all_width = 0;
+			setInterval(function(){
+				$('.x-header__search-row.js-header .promoFilters__root--1Vc6E').find('li').each(function(index, el) {
+					all_width = all_width + $(el).outerWidth()+13;
+				});
+				var realAll_width = all_width ;
+				if(realAll_width <= $('.x-header__search-row.js-header .promoFilters__list--2qiSG').outerWidth()){
+					$('.x-header__search-row.js-header').find('.ab-slider-arrow.left-arrow').css('display','none');
+					$('.x-header__search-row.js-header').find('.ab-slider-arrow.right-arrow').css('display','none');
+					$('.x-header__search-row.js-header .promoFilters__root--1Vc6E ul').css('transform','unset');
+
+				}else{
+					var space = $('.x-header__search-row.js-header .promoFilters__root--1Vc6E ul').outerWidth()  + $('.x-header__search-row.js-header .promoFilters__root--1Vc6E ul').offset().left - $('.x-header__search-row.js-header .ab-test-slider').outerWidth() + 20;
+					$('.x-header__search-row.js-header .promoFilters__root--1Vc6E .promoFilters__list--2qiSG').css({"margin":"0"})
+					$('.x-header__search-row.js-header .ab-test-slider').css({"text-align":"left", "padding":"0"})
+					if(trPos > -21){
+						$('.x-header__search-row.js-header').find('.ab-slider-arrow.left-arrow').css('display','none');
+						$('.x-header__search-row.js-header').find('.ab-slider-arrow.right-arrow').attr('style','');
+					}else if(space == 0){
+						if(space == 0){
+							$('.x-header__search-row.js-header').find('.ab-slider-arrow.left-arrow').attr('style','');
+							$('.x-header__search-row.js-header').find('.ab-slider-arrow.right-arrow').css('display','none');
+						}
+					}else{
+							$('.x-header__search-row.js-header').find('.ab-slider-arrow.left-arrow').attr('style','');
+							$('.x-header__search-row.js-header').find('.ab-slider-arrow.right-arrow').attr('style','');
+					}
+				} 
+			}, 50) 
+			setInterval(function(){
+				var widthSlider = $('.x-header__container').outerWidth()
+				var widthSliderElem = $('.x-header__search-row.js-header .promoFilters__root--1Vc6E .promoFilters__list--2qiSG').outerWidth()
+				if (widthSlider < widthSliderElem) {
+					$('.x-header__search-row.js-header .ab-test-slider').css({"min-width":"100%", "text-align":"center"})
+					$('.x-header__search-row.js-header .promoFilters__root--1Vc6E .promoFilters__list--2qiSG').css({"margin":"0 auto"})
+				} else {
+					$('.x-header__search-row.js-header .ab-test-slider').css({"min-width":"0px", "text-align":"left"})
+					$('.x-header__search-row.js-header .promoFilters__root--1Vc6E .promoFilters__list--2qiSG').css({"margin":"0"})
+				}
+			}, 50)
+
+			clearInterval(interval);
+		}
+		}, 10)
+
+			var styles = "<style>";
+			styles += `
+			.x-header__search-row.js-header .promoFilters__root--1Vc6E ul{
+				transition-duration: 200ms;
+				position:relative;
+			}
+			.ab-test-slider,.ab-test-slider *,.ab-test-slider *::after, .ab-test-slider::before {
+				box-sizing: border-box;
+			}
+			.x-header__search-row.js-header .promoFilters__root--1Vc6E{
+				overflow: hidden;
+				position: relative;
+			}
+			.x-breadcrumb {
+				margin-top: 40px;
+			}
+			.x-header__container{
+				flex-wrap: wrap;
+			}
+			.x-header__search-row.js-header .promoFilters__root--1Vc6E {
+				margin: 0;
+				padding: 0;
+				border: none;
+			}
+			.x-header__search-row.js-header .promoFilters__root--1Vc6E .promoFilters__list--2qiSG {
+				margin: 0;
+			    padding: 7px 0;
+				height: 44px;
+				box-sizing: border-box;
+				display: flex;
+			}
+			.x-header__search-row.js-header .promoFilters__root--1Vc6E .promoFilter__root--2p4V4{
+				margin: 0 0 0 13px;
+				flex: 0 0 auto;
+			}
+			.x-header__search-row.js-header .promoFilters__root--1Vc6E .promoFilter__root--2p4V4:first-child {
+				margin-left: 2px;
+			}
+			.x-header__search-row.js-header {
+				flex-wrap: wrap;
+			}
+			.x-header__search-row.js-header .wrap-ab-test-slider {
+				background-color: white;
+				box-shadow: 0px 4px 20px rgba(38, 25, 47, 0.18);
+				display: block;
+				width: 100%;
+			}
+			.ab-test-slider .promoFilter__close--Y0BNJ {
+				top: -6px;
+			}
+			.ab-test-slider .promoFilter__content--t_x1a {
+				height: 30px;
+				border-radius: 100px;
+				color: #333;
+				border-color: #f5c002;
+			}
+			.ab-test-slider .promoFilter__content--t_x1a:hover {
+				background-color: #f6f8fd;
+			}
+			.x-header__search-row.js-header .ab-test-slider {
+				position: relative;
+				padding: 0 20px;
+				margin: auto;
+			}
+			.ab-slider-arrow {
+				width: 20px;
+				height: 44px;
+				position: absolute;
+				top: 0;
+				display: inline-flex;
+				align-items: center;
+				justify-content: center;
+				background: #DFE1EF;
+				cursor: pointer;
+				z-index: 2;
+			}
+			.ab-slider-arrow:before {
+				position: absolute;
+				top: 0;
+				bottom: 0;
+				width: 50px;
+				pointer-events: none;
+				content: '';
+			}
+			.ab-slider-arrow.left-arrow{
+				left: 0;
+			}
+			.ab-slider-arrow.left-arrow svg{
+				transform: rotate(180deg);
+			}
+			.ab-slider-arrow.right-arrow{
+				right: 0;
+			}
+			.ab-slider-arrow.left-arrow:before{
+				background: linear-gradient(to right,#f6f8fd 0,rgba(246,248,253,0) 100%);
+				left: 100%;
+			}
+			.ab-slider-arrow.right-arrow:before{
+				background: linear-gradient(to left,#f6f8fd 0,rgba(246,248,253,0) 100%);
+				right: 100%;
+			}
+			.x-header__search-row.js-header .ab-test-slider {
+				width: 998px;
+			}
+			@media only screen and (min-width: 1240px) {
+				.x-header__search-row.js-header .ab-test-slider {
+					width: 1240px;
+				}
+			}
+			@media only screen and (min-width: 1500px) {
+				.x-header__search-row.js-header .ab-test-slider {
+					width: 1482px;
+				}
+			}
+			`;
+			styles    += "</style>";
+			jQuery('body').append(styles);
+	})
+}
